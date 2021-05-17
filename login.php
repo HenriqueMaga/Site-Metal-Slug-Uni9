667f755f6f9a4586f65e62b87eaf7c259b959dd7
Henrique Magalhães <henrique.araujo@saint-gobain.com>
2021-05-17 14:40:01 -0300

    Correções do index na página de login

--- login.php
+++ login.php
@@ -12,6 +12,8 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
     integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
     crossorigin="anonymous" />
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 
 <body class="login">
@@ -49,11 +51,12 @@
                 echo '<li class="nav-item"> <a class="nav-link" href="login.php">Login</a> </li>';
               }
             ?>
-
+            
             <?php
 
               if($_SESSION["id"] != null){
                 echo '<li class="nav-item"><a class="nav-link">Bem vindo, '.$_SESSION["name"]. '</a></li>' ;
+                echo '<li class="nav-item"><a class="nav-link">Sair</a></li>' ;
               }
             ?>
         
