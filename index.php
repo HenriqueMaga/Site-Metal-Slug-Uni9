5bd748bd37abbf5de249144f3940b643b0a01a35
Henrique Magalhães <henrique.araujo@saint-gobain.com>
2021-05-17 14:38:47 -0300

    Navbar funcionando e com valores de sessão

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
   <link rel="stylesheet" href="style.css">
   <title>Metal Slug</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -52,6 +54,7 @@
 
               if($_SESSION["id"] != null){
                 echo '<li class="nav-item"><a class="nav-link">Bem vindo, '.$_SESSION["name"]. '</a></li>' ;
+                echo '<li class="nav-item"><a class="nav-link">Sair</a></li>' ;
               }
             ?>
             
