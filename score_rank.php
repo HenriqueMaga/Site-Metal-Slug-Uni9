0339a58d3ea8f146f31a1d16db1c3c7b2c0ee07e
Henrique Magalhães <henrique.araujo@saint-gobain.com>
2021-05-18 16:36:20 -0300

    Icones de rede social vão para páginas da uninove

--- score_rank.php
+++ score_rank.php
@@ -157,14 +157,15 @@
             Metal Slug 2021 - <i class="far fa-copyright align-self-center pd-l-10 pd-r-10" aria-hidden="true"></i>
             Feito com amor em SP!
           </div>
-          <div class="col-md-6 col-lg-7 text-center text-md-right icons"><a href="https://www.facebook.com/astrocodebr/"
+          <div class="col-md-6 col-lg-7 text-center text-md-right icons"><a href="https://pt-br.facebook.com/uninoveoficial/"
               class="fb-ic" rel="noopener noreferrer" target="_blank"><i class="fab fa-facebook-f white-text mr-4"
-                aria-hidden="true"></i></a><a href="/" class="tw-ic" target="_blank" rel="noopener noreferrer"><i
+                aria-hidden="true"></i></a><a href="https://twitter.com/uninoveoficial?lang=en" class="tw-ic" target="_blank" rel="noopener noreferrer"><i
                 class="fab fa-twitter white-text mr-4" aria-hidden="true"> </i></a><a
-              href="https://www.linkedin.com/company/astrocode" target="_blank" rel="noopener noreferrer"
+              href="https://www.linkedin.com/school/uninove/" target="_blank" rel="noopener noreferrer"
               class="li-ic"><i class="fab fa-linkedin-in white-text mr-4" aria-hidden="true"> </i></a><a
-              href="https://www.instagram.com/astrocodebr/" target="_blank" rel="noopener noreferrer" class="ins-ic"><i
-                class="fab fa-instagram white-text" aria-hidden="true"> </i></a></div>
+              href="https://www.instagram.com/uninoveoficial/?hl=en" target="_blank" rel="noopener noreferrer" class="ins-ic"><i
+                class="fab fa-instagram white-text" aria-hidden="true"> </i></a>
+          </div>
         </div>
       </div>
     </div>
